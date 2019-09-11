0728aaafabd8f615b9426e2c2e4bf2f8284a9d72
cdiazarze <cdiaz1@miuandes.cl>
2019-09-11 11:00:47 -0300

    first commit

---
+++ Generic_LINQ/CustomList.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generic_LINQ
+{
+    public class CustomList<T>
+    {
+        private T[] items;
+        private int count;
+
+        public T[] Items { get => items; set => items = value; }
+        public int Count { get => count; private set => count = value; }
+
+        public T this[int index]
+        {
+            get
+            {
+                return Items[index];
+            }
+            set
+            {
+                Items[index] = value;
+            }
+        }
+        public CustomList()
+        {
+            Items = new T[1];
+            Count = 0;
+        }
+
+        public CustomList(T[] elements)
+        {
+            Items = elements.Clone() as T[];
+            Count = elements.Length;
+        }
+
+        public void Append(T element)
+        {
+            T[] tempArray = Items.Clone() as T[];
+            Items = new T[Count + 1];
+            tempArray.CopyTo(Items, 0);
+            Items[Count] = element;
+            Count += 1;
+        }
+
+        public void Prepend(T element)
+        {
+            T[] tempArray = Items.Clone() as T[];
+            Items = new T[Count + 2];
+            tempArray.CopyTo(Items, 1);
+            Items[0] = element;
+            Count += 1;
+        }
+
+        public void Pop()
+        {
+            if (Count > 0)
+            {
+                T[] tempArray = Items.Clone() as T[];
+                Items = new T[Count - 1];
+                Array.Copy(tempArray, 0, Items, 0, Count - 1);
+                Count -= 1;
+            }
+        }
+
+        public override string ToString()
+        {
+            string stringPrint = "[";
+            for (int i = 0; i < Count - 1; i++)
+            {
+                stringPrint += $"{Items[i]}, ";
+            }
+            stringPrint += $"{Items[Count - 1]}]";
+
+            return stringPrint;
+        }
+
+    }
+}
---
+++ Generic_LINQ/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Generic_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomList<int> intList = new CustomList<int>();
+            CustomList<string> stringList = new CustomList<string>(new string[] { "hello", "world" });
+
+            Console.WriteLine(intList.Count);
+            Console.WriteLine(stringList.Count);
+            for (int i = 0; i < 10; i++)
+            {
+                intList.Prepend(i);
+            }
+            Console.WriteLine(intList.ToString());
+            Console.WriteLine(intList[0]);
+            Console.WriteLine(getelement<int>(0, intList));
+
+            //LINQ
+
+            string sentence = "I love cats";
+            string[] catNames = { "Lucky", "Bella", "Luna", "Oreo", "Simba", "Toby", "Loki", "Oscar" };
+            int[] numbers = { 5, 6, 3, 2, 1, 5, 6, 7, 8, 4, 234, 54, 14, 653, 3, 4, 5, 6, 7 };
+
+            // 1. Simple Linq Example
+            var simpleLinq = from s in catNames
+                             select s;
+
+            Console.WriteLine(string.Join("", simpleLinq));
+
+            // 2. Linq Example with Condition
+            var lessThanFive = from num in numbers
+                               where num < 5
+                               select num;
+
+            Console.WriteLine(string.Join(", ", lessThanFive));
+
+            // 3. Linq Example with Multiple Conditions
+            var lessThanFiveAndGreaterThanTen = from num in numbers
+                                                where (num > 5) && (num < 10)
+                                                select num;
+
+            Console.WriteLine(string.Join(", ", lessThanFiveAndGreaterThanTen));
+
+            // 4. Linq Example with Contains
+
+            var catsWithA = from cat in catNames
+                            where cat.Contains("a")
+                            select cat;
+
+            Console.WriteLine(string.Join(", ", catsWithA));
+
+            // 5. Linq Example with Multiple Where
+
+            var moreSpecificCats = from cat in catNames
+                                   where cat.Contains("a")
+                                   where cat.Length > 4
+                                   select cat;
+
+            Console.WriteLine(string.Join(", ", moreSpecificCats));
+
+            // 6. Linq Example with Ordering
+
+            var orderedNumbers = from num in numbers
+                                 where (num > 5) && (num < 10)
+                                 orderby num // optional argument ascending or descending, ascending by default
+                                 select num;
+
+            Console.WriteLine(string.Join(", ", orderedNumbers));
+        }
+
+        public static T getelement<T>(int index, CustomList<T> someList)
+        {
+            return someList[index];
+        }
+    }
+}
